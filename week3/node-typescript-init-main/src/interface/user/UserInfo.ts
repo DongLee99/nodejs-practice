14898d562319036f189cdd9c48ede3e645f20174
DongLee99 <55686088+DongLee99@users.noreply.github.com>
2022-05-11 14:42:57 +0900

    week3 보일러 플레이트

---
+++ week3/node-typescript-init-main/src/interface/user/UserInfo.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import { SchoolInfo } from "../school/SchoolInfo";
+
+export interface UserInfo {
+    name : string;
+    phone: string;
+    email: string;
+    age: string;
+    school: SchoolInfo;
+}
